cb775eb9380f1e9879877cec6288d5e1b035bed2
Samuel Lowry <samlowry@e-baka.net>
2010-01-20 18:23:12 +0700

    Change templates location

--- settings.py
+++ settings.py
@@ -56,7 +56,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'allswingersclubs.urls'
 
 TEMPLATE_DIRS = (
-	# 'templates',
+	'templates',
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
