172a5622a1b188341a1a4ea8fe3647425b2fab8e
Samuel Lowry <samlowry@e-baka.net>
2010-01-21 14:32:53 +0700

    Fix issue with Tradelink.description field size

--- linkator/models.py
+++ linkator/models.py
@@ -13,7 +13,7 @@ class Page(models.Model):
 class Tradelink(models.Model):
 	url = models.URLField('Partner\'s link', verify_exists=False)
 	title = models.CharField('Text of the link', max_length=100)
-	description = models.CharField('Additional description', max_length=300, blank=True)
+	description = models.CharField('Additional description', max_length=255, blank=True)
 	page = models.ForeignKey(Page)
 	
 	class Meta:
