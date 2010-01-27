26940ff3e1545dc5626654c1bc7f923a00c70022
Samuel Lowry <samlowry@e-baka.net>
2010-01-27 22:43:45 +0700

    Move debug_toolbar activation only to dev config (to complicate to configure production httpd for exclude __debug__/*.js now)

--- devel_settings.py
+++ devel_settings.py
@@ -25,3 +25,25 @@ TEMPLATE_DIRS = (
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
 )
+
+#debug_toolbar section (works only for superusers)
+from settings import *
+MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
+INSTALLED_APPS += ('debug_toolbar',)
+DEBUG_TOOLBAR_PANELS = (
+        #'debug_toolbar.panels.version.VersionDebugPanel',
+        'debug_toolbar.panels.timer.TimerDebugPanel',
+        'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+        'debug_toolbar.panels.headers.HeaderDebugPanel',
+        'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+        'debug_toolbar.panels.template.TemplateDebugPanel',
+        'debug_toolbar.panels.sql.SQLDebugPanel',
+        'debug_toolbar.panels.cache.CacheDebugPanel',
+        'debug_toolbar.panels.logger.LoggingPanel',
+    )
+def custom_show_toolbar(request):
+    return request.user.is_superuser
+DEBUG_TOOLBAR_CONFIG = {
+		'INTERCEPT_REDIRECTS': False,
+		'SHOW_TOOLBAR_CALLBACK': custom_show_toolbar,
+    }
