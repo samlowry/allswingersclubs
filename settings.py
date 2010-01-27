75121c63233423ee0097069420a15e22280c9330
Samuel Lowry <samlowry@e-baka.net>
2010-01-27 22:36:24 +0700

    Add debug_toolbar support everywhere, show toolbar for superusers only

--- settings.py
+++ settings.py
@@ -76,10 +76,26 @@ INSTALLED_APPS = (
 	# 'debug_toolbar',
 )
 
-INTERNAL_IPS = ('127.0.0.1',)
+#debug_toolbar section (works only for superusers)
+MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
+INSTALLED_APPS += ('debug_toolbar',)
+DEBUG_TOOLBAR_PANELS = (
+        #'debug_toolbar.panels.version.VersionDebugPanel',
+        'debug_toolbar.panels.timer.TimerDebugPanel',
+        'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+        'debug_toolbar.panels.headers.HeaderDebugPanel',
+        'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+        'debug_toolbar.panels.template.TemplateDebugPanel',
+        'debug_toolbar.panels.sql.SQLDebugPanel',
+        'debug_toolbar.panels.cache.CacheDebugPanel',
+        'debug_toolbar.panels.logger.LoggingPanel',
+    )
+def custom_show_toolbar(request):
+    return request.user.is_superuser
 DEBUG_TOOLBAR_CONFIG = {
-    'INTERCEPT_REDIRECTS': True,
-}
+		'INTERCEPT_REDIRECTS': False,
+		'SHOW_TOOLBAR_CALLBACK': custom_show_toolbar,
+    }
 
 try:
 	from local_settings import *
