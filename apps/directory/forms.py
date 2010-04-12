f2eafa058b2dc3f4b17a3dbf3c735a4935882959
nmb10 <nmb.ten@gmail.com>
2010-04-12 19:45:09 +0400

    changing club form

---
+++ apps/directory/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from directory.models import Club
+
+class ClubForm(forms.ModelForm):
+    class Meta:
+        model = Club
+        fields =  ('name', 'description', 'address', 'email', 'homepage')
+                    # all (name, description, address, state, city, phone, 
+                    # email, homepage, latitude, longitude, rating, 
+                    # date_of_review, is_closed, objects, current_site_only, 
+                    # open_only, sites, owner
