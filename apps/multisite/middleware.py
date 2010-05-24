35418c96cd7e316cf64648efc02930dedb0150bc
Samuel Lowry <samlowry@e-baka.net>
2010-05-24 21:19:14 +0700

    GAN code change to multisite support

--- apps/multisite/middleware.py
+++ apps/multisite/middleware.py
@@ -10,11 +10,11 @@ class DynamicSiteMiddleware(object):
         host = re.sub( '^www\.' , '' , request.get_host() )
         shost = host.rsplit(':', 1)[0] # only host, without port
 
-        try:
-            settings.SITE_ID.set(HOST_CACHE[host])
-            return
-        except KeyError:
-            pass
+        # try:
+        #     settings.SITE_ID.set(HOST_CACHE[host])
+        #     return
+        # except KeyError:
+        #     pass
 
         try: # get by whole hostname
             site = Site.objects.get(domain=host)
@@ -33,17 +33,17 @@ class DynamicSiteMiddleware(object):
             except Site.DoesNotExist:
                 pass
 
-        try: # get by settings.SITE_ID
-            site = Site.objects.get(pk=settings.SITE_ID)
-            HOST_CACHE[host] = site.pk
-            return
-        except Site.DoesNotExist:
-            pass
+        # try: # get by settings.SITE_ID
+        #     site = Site.objects.get(pk=settings.SITE_ID)
+        #     HOST_CACHE[host] = site.pk
+        #     return
+        # except Site.DoesNotExist:
+        #     pass
 
-        try: # misconfigured settings?
-            site = Site.objects.all()[0]
-            HOST_CACHE[host] = site.pk
-            settings.SITE_ID.set(site.pk)
-            return
-        except IndexError: # no sites in db
-            pass
+        # try: # misconfigured settings?
+        #             site = Site.objects.all()[0]
+        #             HOST_CACHE[host] = site.pk
+        #             settings.SITE_ID.set(site.pk)
+        #             return
+        #         except IndexError: # no sites in db
+        #             pass
