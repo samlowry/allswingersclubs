4bec2887892d4d9270b71bc04c9cd9a7ba1fe27c
Samuel Lowry <samlowry@e-baka.net>
2010-05-27 15:42:27 +0700

    Setup feed module layout

--- apps/rss/feeds.py
+++ apps/rss/feeds.py
@@ -2,75 +2,71 @@ from django.contrib.syndication.feeds import Feed
 # works only for django before 1.2
 from django.contrib.syndication.feeds import FeedDoesNotExist
 from django.core.exceptions import ObjectDoesNotExist
-
+from django.contrib.sites.models import Site
 from news.models import News
 from directory.models import Club
 from directory.models import State
 
 
 class LatestNews(Feed):
-    title = "site news"
-    link = "/sitenews/"
-    description = "News about clubs"
+    site = Site.objects.get_current()
+    title = "%s news" % site.name
+    link = "/"
+    description = "News of all clubs, listed in %s directory" % site.name
     
     title_template = "feeds/news_title.html"
     description_template = "feeds/news_description.html"
     
     def items(self):
         return News.objects.order_by('-created')[:10]
-        
-class ClubNews(Feed):
 
+class ClubNews(Feed):    
     title_template = "feeds/news_title.html"
     description_template = "feeds/news_description.html"
-
+    
     def title(self, obj):
         return "%s news feed" % obj.name
-        
+    
     def get_object(self, bits):
         """returns feed object that is using to filter items"""
         if len(bits) != 1:
             raise ObjectDoesNotExist
         return Club.objects.get(id__exact=bits[0])
-
+    
     def description(self, obj):
         return "%s news feed" % obj.name
-        
+    
     def link(self, obj):
         if not obj:
             raise FeedDoesNotExist
         return obj.get_absolute_url()
-        
+    
     def items(self, obj):
         return News.objects.filter(club__id__exact=obj.id).order_by('-created')[:10]
-
         
-class StateNews(Feed):
 
+class StateNews(Feed):
+    
     title_template = "feeds/news_title.html"
-    description_template = "feeds/news_description.html"    
+    description_template = "feeds/news_description.html"
     
     def get_object(self, bits):
         if len(bits) != 1:
             raise ObjectDoesNotExist
-        # get state usps name from bits [0]            
-        if bits[0].endswith("-clubs.html"):
-            usps = bits[0][:2] 
-        else:
-            raise ObjectDoesNotExist
+        usps = bits[0]
         return State.objects.get(usps_name__iexact=usps)
-
+    
     def title(self, obj):
         return "%s state feed" % obj.name
-        
+    
     def description(self, obj):
         return "%s state feed" % obj.name
-
+    
     def link(self, obj):
         if not obj:
             raise FeedDoesNotExist
-        return obj.get_absolute_url()        
-        
+        return obj.get_absolute_url()
+    
     def items(self, obj):
         return News.objects.filter(club__state__id__exact=obj.id).order_by('-created')[:10]
         
---
+++ apps/rss/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls.defaults import *
+from rss.feeds import *
+
+feeds = {
+    'site': LatestNews,
+    'club': ClubNews,
+    'state': StateNews
+}
+
+urlpatterns = patterns('',
+
+                        url(r'(?P<url>.*)/$',
+                            'django.contrib.syndication.views.feed', 
+                            {'feed_dict': feeds}, 
+                            name="news_feed"),
+                           
+)
