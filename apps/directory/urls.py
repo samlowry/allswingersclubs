ba8f2fcc0cd2d2fddd6170ab579dcde2952fcb70
andr0s <no.andrey@gmail.com>
2010-06-14 22:45:58 +0800

    bugfix for country absolute urls

--- apps/directory/urls.py
+++ apps/directory/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('directory.views',
 	(r'^$', 'index'),
-	(r'^country/(?P<country_name>[a-zA-Z]+)/$', 'country'),
+	(r'^country/(?P<country_name>[a-zA-Z -]+)/$', 'country'),
 	(r'^(?P<state_usps_name>[a-z]{2})-clubs\.html$', 'state'),
 	(r'^club/(?P<club_id>\d+)/(?:(?P<club_urlsafe_title>[\w-]+)\.html)?$', 'club'),
 	(r'^club/change/(?P<club_id>\d+)/$', 'change_club'),
