97d2ecda579eafdb7c51357fff492fd599e5bc80
nmb10 <nmb.ten@gmail.com>
2010-07-07 22:18:25 +0400

    country added to the club's change form in the admin panel. filter cities by country when country changes(AJAX).

--- apps/directory/admin.py
+++ apps/directory/admin.py
@@ -1,9 +1,12 @@
-from django.contrib import admin
-from directory.models import *
-from django.db.models.fields import FieldDoesNotExist
 import copy
+
+from django.contrib import admin
+from django.db.models.fields import FieldDoesNotExist
 from django.db.models.fields.related import ForeignKey
 from django.db.models import options
+
+from directory.models import *
+from directory.forms import ClubAdminForm
 
 def get_field(self, name, many_to_many=True):
 	"""
@@ -35,101 +38,108 @@ setattr(options.Options, '_get_field', options.Options.get_field.im_func)
 setattr(options.Options, 'get_field', get_field)
 
 class PhotosInline(admin.TabularInline):
-    model = Photo
-    extra = 0
-    max_num = 15
+	model = Photo
+	extra = 0
+	max_num = 15
 
 class ClubInline(admin.TabularInline):
-    model = Club
-    extra = 0
-    max_num = 15
+	model = Club
+	extra = 0
+	max_num = 15
 
 class CityInline(admin.TabularInline):
-    model = City
-    max_num = 15
-    
-    
+	model = City
+	max_num = 15
+	
+	
 class RegionAdmin(admin.ModelAdmin):
-    list_display = ('name',)
-    search_fields = ('name',)
+	list_display = ('name',)
+	search_fields = ('name',)
 
 
 class CountryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'region',)
-    search_fields = ('name', 'region__name')
-    inlines = [CityInline,]
-    
+	list_display = ('name', 'region',)
+	search_fields = ('name', 'region__name')
+	inlines = [CityInline,]
+	
 
 class CityAdmin(admin.ModelAdmin):
-    inlines = [ClubInline,]
-    list_display = ('id', 'name', 'state_name', 'country_name')
-    list_display_links = ('id', 'name')
-    list_filter = ('state', 'country')
-    search_fields = ('name',)
-    list_select_related = True
+	inlines = [ClubInline,]
+	list_display = ('id', 'name', 'state_name', 'country_name')
+	list_display_links = ('id', 'name')
+	list_filter = ('state', 'country')
+	search_fields = ('name',)
+	list_select_related = True
 
 class StateAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'usps_name')
-    list_display_links = ('id', 'name',)
-    inlines = [CityInline,]
+	list_display = ('id', 'name', 'usps_name')
+	list_display_links = ('id', 'name',)
+	inlines = [CityInline,]
 
 class ClubAdmin(admin.ModelAdmin):
-    inlines = [PhotosInline,]
-    list_display = ('id', 'name', 'owner', 'city_name', 'state_name', 'country_name', 'short_description', 'is_closed', 'homepage_url', 'all_sites')
-    list_display_links = ('id', 'name')
-    list_editable = ('is_closed', )
-    list_filter = ('sites', 'is_closed', 'state', 'city__country')
-    list_per_page = 20
-    ordering = ('state',)
-    search_fields = ('id', 'name', 'city__name', 'city__country__name', 'email')
-    actions_on_bottom = True
-    
-    
-    def queryset(self, request):
-        q = request.GET.copy()
-        
-        # set filter for 'All' link. need to defferetiate All link click and empty request
-        q["id__gt"] = 0
-        
-        # get state is from request
-        state_id = int(request.GET.get("state__id__exact", 0))
-        
-        # get all states filter. if GET contains 'id__gt' then it was All link
-        # if not, it was empty request
-        all_states = request.GET.get("id__gt", None)
+	inlines = [PhotosInline,]
+	list_display = ('id', 'name', 'owner', 'city_name', 'state_name', 'country_name', 'short_description', 'is_closed', 'homepage_url', 'all_sites')
+	list_display_links = ('id', 'name')
+	list_editable = ('is_closed', )
+	list_filter = ('sites', 'is_closed', 'state', 'city__country')
+	list_per_page = 20
+	ordering = ('state',)
+	search_fields = ('id', 'name', 'city__name', 'city__country__name', 'email')
+	actions_on_bottom = True
+	form = ClubAdminForm
+	fieldsets = [
+				(None, {'fields': ['name', 'description', 'owner']}),
+				(None, {'fields': ['address', 'country', 'state', 'city', 'phone', 
+									'email', 'homepage', 'latitude', 'longitude']}),
+				(None, {'fields': ['rating']}),
+				(None, {'fields': ['date_of_review', 'is_closed']}),
+				(None, {'fields': ['sites']}),
+				]
+	def queryset(self, request):
+		q = request.GET.copy()
+		
+		# set filter for 'All' link. need to defferetiate All link click and empty request
+		q["id__gt"] = 0
+		
+		# get state is from request
+		state_id = int(request.GET.get("state__id__exact", 0))
+		
+		# get all states filter. if GET contains 'id__gt' then it was All link
+		# if not, it was empty request
+		all_states = request.GET.get("id__gt", None)
 
-        if state_id:
-            # get state id from session key
-            session_state_id = int(request.session.get('state_id', 0))
-            if session_state_id != state_id:
-                # if they are not the same, replace session key with state id from request
-                request.session["state_id"] = state_id
-        else:
-            # if there is no state
-            if all_states == None:
-                # if it's empty request update GET dict
-                if request.session.has_key("state_id"):
-                    q["state__id__exact"] = request.session["state_id"]
-        
-        request.GET = q
-        qs = super(ClubAdmin, self).queryset(request)
-        return qs.select_related('state', 'city')
-        
-    
-    
-    
-    def formfield_for_foreignkey(self, db_field, request, **kwargs):
-        if db_field.name == 'city':
-            kwargs["queryset"] = City.objects.all().select_related('state')
-            # return db_field.formfield(**kwargs)
-        return super(ClubAdmin, self).formfield_for_foreignkey(db_field, request = request, **kwargs)
-    
+		if state_id:
+			# get state id from session key
+			session_state_id = int(request.session.get('state_id', 0))
+			if session_state_id != state_id:
+				# if they are not the same, replace session key with state id from request
+				request.session["state_id"] = state_id
+		else:
+			# if there is no state
+			if all_states == None:
+				# if it's empty request update GET dict
+				if request.session.has_key("state_id"):
+					q["state__id__exact"] = request.session["state_id"]
+		
+		request.GET = q
+		qs = super(ClubAdmin, self).queryset(request)
+		return qs.select_related('state', 'city')
+		
+	
+	
+	
+	def formfield_for_foreignkey(self, db_field, request, **kwargs):
+		if db_field.name == 'city':
+			kwargs["queryset"] = City.objects.all().select_related('state')
+			# return db_field.formfield(**kwargs)
+		return super(ClubAdmin, self).formfield_for_foreignkey(db_field, request = request, **kwargs)
+	
 
 class PhotoAdmin(admin.ModelAdmin):
-    list_display = ('id', 'club', 'original_image', 'admin_thumbnail_view')
-    list_select_related = True
-    search_fields = ('id', 'original_image')
-    list_per_page = 10
+	list_display = ('id', 'club', 'original_image', 'admin_thumbnail_view')
+	list_select_related = True
+	search_fields = ('id', 'original_image')
+	list_per_page = 10
 
 
 admin.site.register(State, StateAdmin)
--- apps/directory/forms.py
+++ apps/directory/forms.py
@@ -6,61 +6,71 @@ from django.contrib.sites.models import Site
 from directory.models import *
 
 class PhotoForm(forms.ModelForm):
-    
-    class Meta:
-        model = Photo
-    def __init__(self, *args, **kwargs):
-        super(PhotoForm,self ).__init__(*args,**kwargs) 
-        if self.instance.id:
-            self.show_url = self.instance.thumbnail_image.url
-        
+	
+	class Meta:
+		model = Photo
+	def __init__(self, *args, **kwargs):
+		super(PhotoForm,self ).__init__(*args,**kwargs) 
+		if self.instance.id:
+			self.show_url = self.instance.thumbnail_image.url
+		
 
+class ClubAdminForm(forms.ModelForm):
+	country = forms.ModelChoiceField(queryset=Country.objects.all(), required=False)
+	class Meta:
+		model = Club
+		
+	def __init__(self, *args, **kwargs):
+		super(ClubAdminForm, self).__init__(*args, **kwargs)		
+		if self.instance.city:
+			self.fields["country"].initial = self.instance.city.country.id
 
+		
 class ClubForm(forms.ModelForm):
-    # make coord read only.
-    longitude = forms.DecimalField(required=False, widget=TextInput(attrs={'readonly': True}))
-    latitude = forms.DecimalField(required=False, widget=TextInput(attrs={'readonly': True}))
-    
-    # replace sites multiple choice with checkbox
-    sites = forms.ModelMultipleChoiceField(Site.objects.all(), widget=forms.CheckboxSelectMultiple(), required=True)
-    
-    country = forms.ModelChoiceField(queryset=Country.objects.all(), required=False)
+	# make coord read only.
+	longitude = forms.DecimalField(required=False, widget=TextInput(attrs={'readonly': True}))
+	latitude = forms.DecimalField(required=False, widget=TextInput(attrs={'readonly': True}))
+	
+	# replace sites multiple choice with checkbox
+	sites = forms.ModelMultipleChoiceField(Site.objects.all(), widget=forms.CheckboxSelectMultiple(), required=True)
+	
+	country = forms.ModelChoiceField(queryset=Country.objects.all(), required=False)
 
-    class Meta:
-        model = Club
-        #fields =  ('name', 'description', 'address', 'email', 'homepage')
-                    # all (name, description, address, state, city, phone, 
-                    # email, homepage, latitude, longitude, rating, 
-                    # date_of_review, is_closed, objects, current_site_only, 
-                    # open_only, sites, owner
-        exclude = ('owner', 'date_of_review', 'rating', 'is_closed')
-        
+	class Meta:
+		model = Club
+		#fields =  ('name', 'description', 'address', 'email', 'homepage')
+					# all (name, description, address, state, city, phone, 
+					# email, homepage, latitude, longitude, rating, 
+					# date_of_review, is_closed, objects, current_site_only, 
+					# open_only, sites, owner
+		exclude = ('owner', 'date_of_review', 'rating', 'is_closed')
+		
 
-    def __init__(self, *args, **kwargs):
-        super(ClubForm,self ).__init__(*args,**kwargs) 
+	def __init__(self, *args, **kwargs):
+		super(ClubForm,self ).__init__(*args,**kwargs) 
 
-        # if form is bound, it contains state id. filter cities by this state.
-        if self.is_bound:
-            state_id = self.data.get("state", None) 
-            country_id = self.data.get("country", None) 
-            if state_id:
-                state = get_object_or_404(State, id=state_id)
-                self.fields["city"].queryset = state.city_set.all()
-            elif country_id:
-                country = get_object_or_404(Country, id=country_id)
-                self.fields["city"].queryset = country.country_cities.all()
-            else:
-                self.fields["city"].queryset = City.objects.none()
-                
-        else:
-            # club is saved already. filter cities by club's state
-            club = kwargs.get("instance", None)
-            if club:
-                if club.state:
-                    self.fields["city"].queryset = club.state.city_set.all()
-                elif club.country:
-                    self.fields["city"].queryset = club.city.country.country_cities.all()
-            else:
-                self.fields["city"].widget.attrs["disabled"] = True
-                self.fields["city"].queryset = City.objects.none()
-    
+		# if form is bound, it contains state id. filter cities by this state.
+		if self.is_bound:
+			state_id = self.data.get("state", None) 
+			country_id = self.data.get("country", None) 
+			if state_id:
+				state = get_object_or_404(State, id=state_id)
+				self.fields["city"].queryset = state.city_set.all()
+			elif country_id:
+				country = get_object_or_404(Country, id=country_id)
+				self.fields["city"].queryset = country.country_cities.all()
+			else:
+				self.fields["city"].queryset = City.objects.none()
+				
+		else:
+			# club is saved already. filter cities by club's state
+			club = kwargs.get("instance", None)
+			if club:
+				if club.state:
+					self.fields["city"].queryset = club.state.city_set.all()
+				elif club.country:
+					self.fields["city"].queryset = club.city.country.country_cities.all()
+			else:
+				self.fields["city"].widget.attrs["disabled"] = True
+				self.fields["city"].queryset = City.objects.none()
+	
