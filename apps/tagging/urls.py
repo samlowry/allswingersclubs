0aeba721f7520ded9d27a625c185d8852d495f8e
nmb10 <nmb.ten@gmail.com>
2010-08-08 13:41:10 +0400

    urls for tags with "/" fix.

--- apps/tagging/urls.py
+++ apps/tagging/urls.py
@@ -4,13 +4,12 @@ from tagging.views import state_clubs
 from tagging.views import country_clubs
 
 urlpatterns = patterns('',
-                        url(r'usa/(?P<usps>[a-zA-Z]{2})/(?P<tag_name>[a-zA-Z0-9-]*)/$',
-                       #url(r'usa.*/$',
+                        url(r'usa/(?P<usps>[a-zA-Z]{2})/(?P<tag_name>[a-zA-Z0-9-/]*)/$',
                            state_clubs,
                            {'template_name': 'tagging/region_clubs.html'},
                            name='state_clubs'), 
 
-                        url(r'(?P<country_name>[a-zA-Z0-9-_]*)/(?P<tag_name>[a-zA-Z0-9-]*)/$',
+                        url(r'(?P<country_name>[a-zA-Z0-9-_]*)/(?P<tag_name>[a-zA-Z0-9-/]*)/$',
                             country_clubs,
                             {'template_name': 'tagging/region_clubs.html'},
                             name='country_clubs'),
