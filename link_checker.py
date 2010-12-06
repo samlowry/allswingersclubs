4bd0ad678343c524326a9ec836c59a2fea92d7d8
Samuel Lowry <samlowry@e-baka.net>
2010-12-06 16:25:41 +0700

    Fixed some issues in linkchecker

--- link_checker.py
+++ link_checker.py
@@ -1,10 +1,7 @@
 #-*- coding: utf-8 -*-
-import sys, os, string
-import socket, urllib, urllib2, httplib
-import thread
-from signal import SIGTERM
-import simplejson
-import socket, re
+import sys, os
+import urllib2
+
 
 os.environ["DJANGO_SETTINGS_MODULE"] = 'settings'
 
@@ -12,13 +9,13 @@ from settings import *
 from directory.models import *
 
 
-CLUBS = Club.objects.all()
-# CLUBS = Club.objects.filter(id__in=[817, 591, 788, 946, 836])
+# CLUBS = Club.objects.all()
+CLUBS = Club.objects.filter(id__in=[817, 591, 788, 946, 836])
 for club in CLUBS:
     if club.homepage:
         try:
             urllib2.urlopen(club.homepage)
         except Exception, error:
-            club.homepage = null
+            club.homepage = False
             club.save()
             print "%s (%s) has error %s" % (club.id, club.homepage, error)
