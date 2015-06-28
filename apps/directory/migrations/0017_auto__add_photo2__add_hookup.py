8a0cf731cbc22856c4bcc780f5d546f9376a2e5c
Samuel Lowry <samlowry@e-baka.net>
2015-06-28 13:28:29 +0700

    Added migration for Hookups

---
+++ apps/directory/migrations/0017_auto__add_photo2__add_hookup.py
@@ -0,0 +1,223 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Photo2'
+        db.create_table('directory_photo2', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('original_image', self.gf('django.db.models.fields.files.ImageField')(max_length=100)),
+            ('hookup', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['directory.Hookup'])),
+        ))
+        db.send_create_signal('directory', ['Photo2'])
+
+        # Adding model 'Hookup'
+        db.create_table('directory_hookup', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=50)),
+            ('description', self.gf('django.db.models.fields.TextField')(blank=True)),
+            ('address', self.gf('django.db.models.fields.CharField')(max_length=100, blank=True)),
+            ('state', self.gf('django.db.models.fields.related.ForeignKey')(blank=True, related_name='hookups', null=True, to=orm['directory.State'])),
+            ('city', self.gf('django.db.models.fields.related.ForeignKey')(blank=True, related_name='hookups', null=True, to=orm['directory.City'])),
+            ('date_of_publish', self.gf('django.db.models.fields.DateField')(default=datetime.date.today)),
+            ('age', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('body', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('height', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('weight', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('body_art', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('diet', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('dislikes', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('drinks', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('drugs', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('education', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('ethnicity', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('eye_color', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('facial_hair', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('fears', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('hair', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('hiv_hsv_hpv', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('interests', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('kids_have', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('kids_want', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('likes', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('native_language', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('occupation', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('personality', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('pets', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('politics', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('religion', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('resembles', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('smokes', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('zodiac', self.gf('django.db.models.fields.CharField')(max_length=100)),
+        ))
+        db.send_create_signal('directory', ['Hookup'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Photo2'
+        db.delete_table('directory_photo2')
+
+        # Deleting model 'Hookup'
+        db.delete_table('directory_hookup')
+
+
+    models = {
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'directory.city': {
+            'Meta': {'ordering': "['state', 'name']", 'unique_together': "(('state', 'name'),)", 'object_name': 'City'},
+            'country': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'country_cities'", 'null': 'True', 'to': "orm['directory.Country']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'state': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['directory.State']", 'null': 'True', 'blank': 'True'})
+        },
+        'directory.club': {
+            'Meta': {'ordering': "['name']", 'object_name': 'Club'},
+            'address': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
+            'city': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'clubs'", 'null': 'True', 'to': "orm['directory.City']"}),
+            'date_of_review': ('django.db.models.fields.DateField', [], {'default': 'datetime.date.today'}),
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'homepage': ('django.db.models.fields.URLField', [], {'max_length': '200', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_closed': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'latitude': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '10', 'decimal_places': '6', 'blank': 'True'}),
+            'longitude': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '10', 'decimal_places': '6', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'owner': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']", 'null': 'True', 'blank': 'True'}),
+            'phone': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'rating': ('django.db.models.fields.FloatField', [], {'default': '3.0'}),
+            'sites': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['sites.Site']", 'symmetrical': 'False'}),
+            'state': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'clubs'", 'null': 'True', 'to': "orm['directory.State']"})
+        },
+        'directory.country': {
+            'Meta': {'ordering': "('name',)", 'object_name': 'Country'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '50'}),
+            'region': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'region_countries'", 'to': "orm['directory.Region']"}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'})
+        },
+        'directory.countrydescription': {
+            'Meta': {'object_name': 'CountryDescription'},
+            'country': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['directory.Country']"}),
+            'description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'site': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['sites.Site']"})
+        },
+        'directory.hookup': {
+            'Meta': {'ordering': "['title']", 'object_name': 'Hookup'},
+            'address': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
+            'age': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'body': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'body_art': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'city': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'hookups'", 'null': 'True', 'to': "orm['directory.City']"}),
+            'date_of_publish': ('django.db.models.fields.DateField', [], {'default': 'datetime.date.today'}),
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'diet': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'dislikes': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'drinks': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'drugs': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'education': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'ethnicity': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'eye_color': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'facial_hair': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'fears': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'hair': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'height': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'hiv_hsv_hpv': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'interests': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'kids_have': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'kids_want': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'likes': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'native_language': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'occupation': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'personality': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'pets': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'politics': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'religion': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'resembles': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'smokes': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'state': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'hookups'", 'null': 'True', 'to': "orm['directory.State']"}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'weight': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'zodiac': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'directory.photo': {
+            'Meta': {'object_name': 'Photo'},
+            'club': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['directory.Club']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'original_image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'})
+        },
+        'directory.photo2': {
+            'Meta': {'object_name': 'Photo2'},
+            'hookup': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['directory.Hookup']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'original_image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'})
+        },
+        'directory.region': {
+            'Meta': {'ordering': "('name',)", 'object_name': 'Region'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '50'})
+        },
+        'directory.state': {
+            'Meta': {'ordering': "['name']", 'object_name': 'State'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '20'}),
+            'usps_name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '2'})
+        },
+        'directory.statedescription': {
+            'Meta': {'object_name': 'StateDescription'},
+            'description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'site': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['sites.Site']"}),
+            'state': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['directory.State']"})
+        },
+        'sites.site': {
+            'Meta': {'ordering': "('domain',)", 'object_name': 'Site', 'db_table': "'django_site'"},
+            'domain': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        }
+    }
+
+    complete_apps = ['directory']
