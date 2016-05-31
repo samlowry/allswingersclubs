a509dd0978052d1eb915da29ff092bb6df9b2f66
Samuel Lowry <samlowry@e-baka.net>
2016-05-31 18:14:57 +0700

    Debug mode off

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ import sys
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 sys.path.insert(0, os.path.join(PROJECT_ROOT, 'apps'))
 
-DEBUG = True
+DEBUG = False
 
 from multisite.threadlocals import SiteIDHook
 SITE_ID = SiteIDHook()
