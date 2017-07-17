cbaec4248aacee34da527adfeecff85e5200f86a
No Suchi Ip <nosuchip@gmail.com>
2017-07-18 01:55:52 +0700

    Fix fail on missing request

--- apps/linkator/templatetags/linkator_tags.py
+++ apps/linkator/templatetags/linkator_tags.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+
+import re
 from django import template
 from linkator.models import Tradelink
 from django.conf import settings
@@ -5,27 +8,28 @@ from django.db.models import Q
 
 register = template.Library()
 
-class TradelinksNode(template.Node):
-	def __init__(self, var_name):
-		self.var_name = var_name
 
-	def render(self, context):
-		request = context.get('request')
+class TradelinksNode(template.Node):
+    def __init__(self, var_name):
+        self.var_name = var_name
+
+    def render(self, context):
+        request = context.get('request')
         if request:
             context[self.var_name] = Tradelink.objects.filter(page__site__id__exact=settings.SITE_ID).filter( Q(page__path__exact=request.path) | Q(page__path__exact='*') ).order_by('id')
         return ''
 
-import re
-#@register.tag
+
 def get_tradelinks(parser, token):
-	try:
-		tag_name, arg = token.contents.split(None, 1)
-	except ValueError:
-		raise template.TemplateSyntaxError, "%r tag requires arguments" % token.contents.split()[0]
-	m = re.search(r'as (\w+)', arg)
-	if not m:
-		raise template.TemplateSyntaxError, "%r tag had invalid arguments" % tag_name
-	var_name, = m.groups()
-	return TradelinksNode(var_name)
+    try:
+        tag_name, arg = token.contents.split(None, 1)
+    except ValueError:
+        raise template.TemplateSyntaxError, "%r tag requires arguments" % token.contents.split()[0]
+    m = re.search(r'as (\w+)', arg)
+    if not m:
+        raise template.TemplateSyntaxError, "%r tag had invalid arguments" % tag_name
+    var_name, = m.groups()
+    return TradelinksNode(var_name)
+
 
 register.tag(get_tradelinks)
